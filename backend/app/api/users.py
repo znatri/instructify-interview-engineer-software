e59f16f6679c1213524d76533892283531829328
Hardik Goel <hardikkgoel@gmail.com>
2023-09-27 08:55:38 -0400

    feature: Added new api endpoint

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -12,6 +12,8 @@ from app.deps.users import current_superuser
 from app.models.user import User
 from app.schemas.user import UserRead
 
+import requests
+
 router = APIRouter()
 
 
@@ -30,3 +32,33 @@ async def get_users(
     )
     response.headers["Content-Range"] = f"{skip}-{skip + len(users)}/{total}"
     return users
+
+@router.get("/users/valid")
+async def validate_users(
+    response: Response,
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_superuser),
+    skip: int = 0,
+    limit: int = 100
+) -> Any :
+    logger.info("Fetching users")
+    total = await session.scalar(select(func.count(User.id)))
+    users = ((await session.execute(select(User).offset(skip).limit(limit))).scalars().all())
+    logger.info("Validating domain urls")
+    # create a container to store valid users
+    valid_users = []
+    # iterate over users list in O(n)
+    for user in users:
+        try:
+            # implemntation note: find a better method to get domain name, currently it doesn't account for multiple "@" and can be thrown off
+            domain = "https://" + user.email.split("@")[-1] 
+            r = requests.get(domain)
+            if r.status_code == 200:
+                valid_users.append(user)
+        # the following exception was unaccounted for and resulted in the errorr during the live interview
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error validating user: {e}")
+            pass
+    response.headers["Content-Range"] = f"{skip} - {skip + len(valid_users)}/{total}"
+    return valid_users
+    
